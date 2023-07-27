7a66c895149678612075f38088b25837ecd9d909
Björn Svensson <bjorn.a.svensson@est.tech>
2023-07-27 10:01:41 +0200

    Increased freebsd vm memory

--- vm/freebsd/install.sh
+++ vm/freebsd/install.sh
@@ -15,10 +15,13 @@ git config --global alias.st status
 
 # Get source (~7min)
 git clone -b stable/13 https://git.freebsd.org/src.git /usr/src
+git config --global --add safe.directory /usr/src
+
 cd /usr/src
 git config pull.ff only
+
 # or
-# git clone -b stable/13 --depth 1 https://git.freebsd.org/src.git /usr/src
+# git clone https://git.freebsd.org/src.git /usr/src
 # Get all later: git pull --unshallow
 
 #===========================
@@ -26,22 +29,22 @@ git config pull.ff only
 # From: FreeBSD 13.0-STABLE (GENERIC) #0 stable/13-n248936-972796d007c: Thu Jan 13 02:34:59 UTC 2022
 # To:   FreeBSD 13.0-STABLE (GENERIC) #0 stable/13-n249026-eccfee8330e: Tue Jan 18 14:27:10 UTC 2022
 #===========================
-# Compile the new compiler and a few related tools, then use the new compiler
-# to compile the rest of the new world. The result is saved to /usr/obj.
-# ~55min
-#make -j8 buildworld
+## Compile the new compiler and a few related tools, then use the new compiler
+## to compile the rest of the new world. The result is saved to /usr/obj.
+## ~55min
+# make -j8 buildworld
 
-# Use the new compiler residing in /usr/obj to build the new kernel, and install it
-# ~6min, uses ~3GB
-#make -j8 kernel
+## Use the new compiler residing in /usr/obj to build the new kernel, and install it
+## ~6min, uses ~3GB
+# make -j8 kernel
 
-# ~2min
+## ~2min
 # make installworld
 
-# Merge config files
-#mergemaster -Ui
+## Merge config files
+# mergemaster -Ui
 
-# Use new system
-#shutdown -r now
-#cd /usr/src
-#make check-old
+## Use new system
+# shutdown -r now
+# cd /usr/src
+# make check-old
